fffebd9ac735bd83ff7a4bb4b42037dea80aa587
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-12-17 19:00:09 -0500

    Update MainActivity.java
    Request location permission on startup.

--- app/src/main/java/com/example/signmein/MainActivity.java
+++ app/src/main/java/com/example/signmein/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.signmein;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions();
+    }
+
+    private final static int REQUEST_FINE_LOCATION_PERMISSION = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_FINE_LOCATION_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Permission granted.
+                }
+                else {
+                    //Permission is denied. Since location is required, ask again.
+                    //This will behave really badly if "Don't ask again" is selected, repeatedly showing
+                    //the alert box... At least it gets the point across...
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Sorry, but we need permission. ");
+                    builder.setMessage("Android will not permit us to access bluetooth and wifi hardware without location permission. ");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION_PERMISSION);
+                        }
+                    });
+
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    protected void requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("SignMeIn Needs Location Access");
+                builder.setMessage("Bluetooth and Wifi hardware is used by SignMeIn. Since location can be approximated using such hardware, android requires us to ask for permission to access your location. ");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION_PERMISSION);
+                    }
+                });
+
+                builder.show();
+            }
+        }
     }
 }
