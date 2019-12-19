c315e7e9649323f1530154cd202d029e6894edc9
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-12-18 19:08:45 -0500

    Handle "don't ask again" for permissions. Also, import nearby devices package from google play.

--- app/src/main/java/com/example/signmein/MainActivity.java
+++ app/src/main/java/com/example/signmein/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import com.google.android.gms.nearby.connection.Strategy;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestPermissions();
     }
+
+
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+
 
     private final static int REQUEST_FINE_LOCATION_PERMISSION = 0;
 
@@ -31,19 +41,36 @@ public class MainActivity extends AppCompatActivity {
                     //Permission granted.
                 }
                 else {
-                    //Permission is denied. Since location is required, ask again.
-                    //This will behave really badly if "Don't ask again" is selected, repeatedly showing
-                    //the alert box... At least it gets the point across...
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Sorry, but we need permission. ");
-                    builder.setMessage("Android will not permit us to access bluetooth and wifi hardware without location permission. ");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        public void onDismiss(DialogInterface dialog) {
-                            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION_PERMISSION);
-                        }
-                    });
+                    boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
 
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setPositiveButton(android.R.string.ok, null);
+
+                    if (showRationale) {
+                        //Permission is denied. Since location is required, ask again.
+                        builder.setTitle("Sorry, but we need permission. ");
+                        builder.setMessage("Android will not permit us to access bluetooth and wifi hardware without location permission. ");
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            public void onDismiss(DialogInterface dialog) {
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION_PERMISSION);
+                            }
+                        });
+                    }
+                    else {
+                        //Permission has been denied, with "don't ask again" selected.
+                        
+                        builder.setTitle("Location permission is required. ");
+                        builder.setMessage("SignMeIn needs location permission in order to work. Since you have blocked additional permission requests, we will open settings so that you can grant us the permission manually. ");
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            public void onDismiss(DialogInterface dialog) {
+                                //Open our application page in settings.
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivity(intent);
+                            }
+                        });
+                    }
                     builder.show();
                 }
                 return;
@@ -53,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void requestPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //TODO: We should request all permissions in case they are reclassified from normal to dangerous.
 
             if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(this);
