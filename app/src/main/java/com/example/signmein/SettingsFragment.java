7ec8f55fc28f74552eba9c71f14645689fad14e2
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-01-03 16:57:15 -0500

    Clean up Navigation

---
+++ app/src/main/java/com/example/signmein/SettingsFragment.java
@@ -0,0 +1,18 @@
+package com.example.signmein;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Draw client_fragment XML.
+        return inflater.inflate(R.layout.about_fragment, container, false);
+    }
+}
