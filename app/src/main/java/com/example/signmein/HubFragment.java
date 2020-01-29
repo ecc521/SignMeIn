e2fcd66e6066b808f4c4eea268737173e01830dc
imharris-oss <58919114+imharris-oss@users.noreply.github.com>
2020-01-29 15:59:29 -0500

    Began work on hub page backend

--- app/src/main/java/com/example/signmein/HubFragment.java
+++ app/src/main/java/com/example/signmein/HubFragment.java
@@ -1,13 +1,13 @@
 package com.example.signmein;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 public class HubFragment extends Fragment {
@@ -29,5 +29,17 @@ public class HubFragment extends Fragment {
         super.onPause();
         DeviceConnector deviceConnector = new DeviceConnector(getContext());
         deviceConnector.stopAdvertising();
+    }
+
+    public void signIn(View view) {
+        EditText studentName = (EditText) findViewbyId (R.id.studentName);
+        
+        if (studentName.getText().toString().isEmpty()){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(null);
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setTitle("Please enter a name.");
+        } else {
+
+        }
     }
 }
