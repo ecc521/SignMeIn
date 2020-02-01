ecc521/SignMeIn

commit e2fcd66e6066b808f4c4eea268737173e01830dc

Began work on hub page backend

app/build.gradle
@@ -42,7 +42,7 @@ dependencies {
     implementation 'com.google.android.gms:play-services-nearby:17.0.0'
     implementation 'com.google.firebase:firebase-analytics:17.2.0'
     //May need for database, can remove if not needed
-    implementation 'com.google.firebase:firebase-storage:19.1.0'
+    implementation 'com.google.firebase:firebase-firestore:21.3.1'
     /* other FireBase SDKs
     https://firebase.google.com/docs/android/setup#available-libraries
      */

app/src/main/java/com/example/signmein/HubFragment.java
@@ -1,13 +1,13 @@
 package com.example.signmein;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 public class HubFragment extends Fragment {
@@ -29,5 +29,17 @@ public class HubFragment extends Fragment {
         super.onPause();
         DeviceConnector deviceConnector = new DeviceConnector(getContext());
         deviceConnector.stopAdvertising();
+    }
+
+    public void signIn(View view) {
+        EditText studentName = (EditText) findViewbyId (R.id.studentName);
+        
+        if (studentName.getText().toString().isEmpty()){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(null);
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setTitle("Please enter a name.");
+        } else {
+
+        }
     }
 }

app/src/main/res/layout/hub_fragment.xml
@@ -12,7 +12,7 @@
         android:layout_marginStart="24dp"
         android:layout_marginTop="24dp"
         android:layout_marginEnd="24dp"
-        android:text="HubName"
+        android:text="@string/hubname"
         android:textSize="36sp"
         app:layout_constraintEnd_toEndOf="parent"
         app:layout_constraintStart_toStartOf="parent"
@@ -34,13 +34,14 @@
             android:layout_marginTop="16dp"
             android:layout_marginBottom="16dp"
             android:text="Sign Me In"
+            android:onClick="signIn"
             app:layout_constraintBottom_toBottomOf="parent"
             app:layout_constraintEnd_toEndOf="parent"
             app:layout_constraintStart_toStartOf="parent"
-            app:layout_constraintTop_toBottomOf="@+id/editText3" />
+            app:layout_constraintTop_toBottomOf="@+id/studentName" />
 
         <EditText
-            android:id="@+id/editText3"
+            android:id="@+id/studentName"
             android:layout_width="wrap_content"
             android:layout_height="wrap_content"
             android:layout_marginTop="24dp"
@@ -49,7 +50,8 @@
             android:inputType="textPersonName"
             app:layout_constraintEnd_toEndOf="parent"
             app:layout_constraintStart_toStartOf="parent"
-            app:layout_constraintTop_toTopOf="parent" />
+            app:layout_constraintTop_toTopOf="parent"
+            android:importantForAutofill="no" />
     </androidx.constraintlayout.widget.ConstraintLayout>
 
     <TextView
@@ -74,5 +76,78 @@
         app:layout_constraintEnd_toEndOf="parent"
         app:layout_constraintStart_toStartOf="parent"
         app:layout_constraintTop_toBottomOf="@+id/textView8" />
+
+    <LinearLayout
+        android:id="@+id/history"
+        android:layout_width="409dp"
+        android:layout_height="404dp"
+        android:orientation="vertical"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="1.0"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.993">
+
+        <TextView
+            android:id="@+id/name1"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name2"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name3"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name4"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name5"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name6"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name7"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name8"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name9"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+
+        <TextView
+            android:id="@+id/name10"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:textSize="30sp" />
+    </LinearLayout>
 
 </androidx.constraintlayout.widget.ConstraintLayout>

app/src/main/res/values/strings.xml
@@ -22,5 +22,6 @@
     <string name="menu_faq">FAQ</string>
     <string name="menu_about">About</string>
     <string name="menu_settings">Settings</string>
+    <string name="hubname">HubName</string>
 
 </resources>

commit 077a7cc800e755d51a3ce68ce278e5a8f37a40dd

Get Local Sign In UI component working.
Also fixed some bugs caused in commit e2fcd66 that included Null being passed to an @NonNull, and too many methods being in the application dex file.

app/build.gradle
@@ -10,6 +10,7 @@ android {
         versionCode 1
         versionName "1.0"
         testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled true
     }
     buildTypes {
         release {
@@ -40,10 +41,13 @@ dependencies {
     androidTestImplementation 'androidx.test.ext:junit:1.1.0'
     androidTestImplementation 'androidx.test.espresso:espresso-core:3.1.1'
     implementation 'com.google.android.gms:play-services-nearby:17.0.0'
-    implementation 'com.google.firebase:firebase-analytics:17.2.0'
+    implementation 'com.google.firebase:firebase-analytics:17.2.2'
     //May need for database, can remove if not needed
-    implementation 'com.google.firebase:firebase-firestore:21.3.1'
+    implementation 'com.google.firebase:firebase-firestore:21.4.0'
     /* other FireBase SDKs
     https://firebase.google.com/docs/android/setup#available-libraries
      */
+
+    //multidex required because of large amounts of methods in firebase apis.
+    implementation 'com.android.support:multidex:1.0.3'
 }

app/src/main/java/com/example/signmein/HubFragment.java
@@ -1,10 +1,19 @@
 package com.example.signmein;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -16,7 +25,49 @@ public class HubFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.hub_fragment, container, false);
+
+        View inputView = inflater.inflate(R.layout.hub_fragment, container, false);
+
+        //If the user presses the sign me in button, sign them in.
+        Button localSignInButton = inputView.findViewById(R.id.localSignInButton);
+        //localSignInButton.setOnClickListener(this::localSignIn);
+        //localSignInButton.setOnClickListener(this::hideKeyboard);
+        localSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                localSignIn();
+                hideKeyboard();
+            }
+        });
+
+
+        EditText nameInputField = inputView.findViewById(R.id.studentName);
+
+        //When the user presses enter after inputting their name, sign them in.
+        nameInputField.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent keyevent) {
+                //If the keyevent is a key-down event on the "enter" button
+                if ((keyevent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    localSignIn();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        return inputView;
+    }
+
+
+    public void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager)
+                getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow((null == getActivity().getCurrentFocus())
+                ? null : getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        //inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                //InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     public void onResume() {
@@ -31,15 +82,37 @@ public class HubFragment extends Fragment {
         deviceConnector.stopAdvertising();
     }
 
-    public void signIn(View view) {
-        EditText studentName = (EditText) findViewbyId (R.id.studentName);
-        
-        if (studentName.getText().toString().isEmpty()){
-            final AlertDialog.Builder builder = new AlertDialog.Builder(null);
+    public void userSignedIn(String name, String deviceID) {
+        TextView signInHistory = getActivity().findViewById(R.id.history);
+        String text = signInHistory.getText().toString();
+
+        if (!text.contains("\n")) {
+            //Clear the textbox. There is currently nothing in it.
+            text = "";
+        }
+
+        DateFormat dateFormat = DateFormat.getTimeInstance(); //new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        String time = dateFormat.format(date);
+
+        text = name + " signed in at " + time + "\n" + text;
+
+        signInHistory.setText(text);
+    }
+
+    public void localSignIn() {
+        EditText studentName = getActivity().findViewById(R.id.studentName);
+
+        String name = studentName.getText().toString();
+        if (name.isEmpty()){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
             builder.setPositiveButton(android.R.string.ok, null);
             builder.setTitle("Please enter a name.");
-        } else {
-
+            builder.show();
+        }
+        else {
+            userSignedIn(name, "local");
+            studentName.setText("");
         }
     }
 }

app/src/main/res/layout/hub_fragment.xml
@@ -3,7 +3,8 @@
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
     android:layout_width="match_parent"
-    android:layout_height="match_parent">
+    android:layout_height="match_parent"
+    tools:context=".HubFragment">
 
     <TextView
         android:id="@+id/hubName"
@@ -28,13 +29,12 @@
         app:layout_constraintTop_toBottomOf="@+id/hubName">
 
         <Button
-            android:id="@+id/button"
+            android:id="@+id/localSignInButton"
             android:layout_width="wrap_content"
             android:layout_height="wrap_content"
             android:layout_marginTop="16dp"
             android:layout_marginBottom="16dp"
             android:text="Sign Me In"
-            android:onClick="signIn"
             app:layout_constraintBottom_toBottomOf="parent"
             app:layout_constraintEnd_toEndOf="parent"
             app:layout_constraintStart_toStartOf="parent"
@@ -66,7 +66,7 @@
         app:layout_constraintTop_toBottomOf="@+id/constraintLayout2" />
 
     <TextView
-        android:id="@+id/history"
+        android:id="@+id/textView10"
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"
         android:layout_marginStart="16dp"
@@ -76,5 +76,18 @@
         app:layout_constraintEnd_toEndOf="parent"
         app:layout_constraintStart_toStartOf="parent"
         app:layout_constraintTop_toBottomOf="@+id/textView8" />
+
+
+    <TextView
+        android:id="@+id/history"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="16dp"
+        android:layout_marginTop="8dp"
+        android:layout_marginEnd="16dp"
+        android:text="History will appear here... "
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/textView10" />
 
 </androidx.constraintlayout.widget.ConstraintLayout>