b41d563199cd96c9cd659947e0548bd19d92f98c
imharris-oss <58919114+imharris-oss@users.noreply.github.com>
2020-02-12 15:23:48 -0500

    Changed GoogleLogin from an Activity to a Fragment

--- app/src/main/java/com/example/signmein/GoogleLoginFragment.java
+++ app/src/main/java/com/example/signmein/GoogleLoginFragment.java
@@ -1,11 +1,15 @@
 package com.example.signmein;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,28 +18,36 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-public class GoogleLogin extends AppCompatActivity {
+
+public class GoogleLoginFragment extends Fragment {
 
     private static final String TAG = "WARN";
 
+    public GoogleLoginFragment() {
+        // Required empty public constructor
+    }
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_login);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getActivity().getApplicationContext(), gso);
+        getView().findViewById(R.id.sign_in_button).setOnClickListener(this::onClick);
+
+        return inflater.inflate(R.layout.fragment_google_login, container, false);
     }
 
     public void onStart() {
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext());
         updateUI(account);
     }
 
     private void updateUI(GoogleSignInAccount account) {
-        // TODO: Set to lead to Hub choice activity, Pull profile id
+        // TODO: Set to lead to Hub choice fragment, Pull profile id
     }
 
     public void onClick(View v) {
@@ -48,7 +60,7 @@ public class GoogleLogin extends AppCompatActivity {
 
     private void signIn() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getActivity().getApplicationContext(), gso);
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 1);
     }
